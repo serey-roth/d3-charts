c22ee82121484eb9a9ce78be97859f74969e84d9
serey-roth <sereyratanakroth@gmail.com>
2022-11-04 18:18:40 -0700

    add transitions, clean up code, and extract createLegend function for reuse

--- modules/choropleth.js
+++ modules/choropleth.js
@@ -1,15 +1,6 @@
-import { addDescription, addTitle, mousemove ,mouseleave, mouseover } from "./misc";
+import { mousemove ,mouseleave, mouseover, createGradientLegends } from "./misc";
 
-const svg = d3.select('.chart')
-    .append('svg')
-        .attr('viewBox', '0 0 1000 700')
-
-addTitle(svg, 'United States Educational Attainment')
-addDescription(svg, `Percentage of adults age 25 and older with
-a bachelor's degree or higher (2010 - 2014)`)
-
-export const drawChoropleth = (education, usTopoJson) => {
-    //configure path and projection for map
+export const Choropleth = (svg, education, usTopoJson) => {
     const countiesGeoJson = topojson.feature(usTopoJson, 
         usTopoJson.objects.counties)
 
@@ -29,9 +20,9 @@ export const drawChoropleth = (education, usTopoJson) => {
         ${educationInfo.bachelorsOrHigher}%`);
     }
     
-    svg.append('g')
-            .attr('id', 'counties')
-            .attr('transform', 'translate(0, 100)')
+    const map = svg.append('g')
+        .attr('id', 'counties')
+        .attr('transform', 'translate(0, 150)')
         .selectAll('path')
         .data(counties)
         .enter()
@@ -40,56 +31,41 @@ export const drawChoropleth = (education, usTopoJson) => {
             .attr('data-fips', (d) => d.id)
             .attr('data-education', (d, i) => education[i].bachelorsOrHigher)
             .attr('index', (d, i) => i)
-            .attr('d', path)
-            .style('fill', (d, i) => colorsScale(education[i].bachelorsOrHigher))
             .style('stroke', 'white')
             .style('stroke-dasharray', '1,1')
-            .style('stroke-width', 0.5)
+            .style('stroke-width', 0.5)     
         .on('mouseover', mouseover)
         .on('mousemove', move)
         .on('mouseleave', mouseleave)
+    
+    const mapPromise = map.transition()
+        .delay((d, i) => i * 2)
+        .ease(d3.easePolyIn)
+        .attr('d', path)
+        .style('fill', (d, i) => colorsScale(education[i].bachelorsOrHigher))   
+        .end()    
 
     //add borders
-    svg.append('g')
+    const borderPromise = svg.append('g')
             .attr('id', 'borders')
-            .attr('transform', 'translate(0, 100)')
+            .attr('transform', 'translate(0, 150)')
         .append('path')
-        .attr('d', path(topojson.mesh(usTopoJson, 
-            usTopoJson.objects.states, 
-            function(a, b) { return a !== b; })))
         .style('fill', 'none')
         .style('stroke', 'white')
         .style('stroke-width', 1.5)
-        
-    //add color legend
+        .transition()
+            .delay((d, i) => i * 2)
+            .ease(d3.easePolyIn)
+            .attr('d', path(topojson.mesh(usTopoJson, 
+            usTopoJson.objects.states, 
+            function(a, b) { return a !== b; })))  
+        .end()
+
+    
     const legendTicks = d3.scaleLinear()
-    .domain(d3.extent(education, (e) => e.bachelorsOrHigher))
-    .range([0, 200])
-    .nice()
-
-    const legends = svg.append('g')
-        .attr('id', 'legends')
-        .attr('transform', 'translate(700, 100)')
-
-    legends.selectAll('rect')
-    .data(d3.range(...legendTicks.range()))
-    .enter()
-    .append('rect')
-        .attr('x', (d, i) => i)
-        .attr('y', 0)
-        .attr('width', 1)
-        .attr('height', 10)
-        .attr('fill', (d) => colorsScale(legendTicks.invert(d)));
-
-    legends.call(d3.axisBottom(legendTicks)
-        .ticks(5)
-        .tickSize(15)
-        .tickFormat((d) => `${d}%`));
-
-    legends.select('path')
-        .style('stroke', 'none')
-
-    legends.selectAll('line')
-        .style('stroke', 'black')
-
+        .domain(d3.extent(education, (e) => e.bachelorsOrHigher))
+        .range([0, 200])
+        .nice()
+    
+    createGradientLegends(svg, legendTicks, colorsScale, [mapPromise, borderPromise]);
 }
